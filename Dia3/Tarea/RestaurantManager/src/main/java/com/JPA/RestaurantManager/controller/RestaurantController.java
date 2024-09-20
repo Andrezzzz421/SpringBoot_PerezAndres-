ce815fedf0000ce5df749503a9653bf5f0f14ccb
Andres Perez <156430410+Andrezzzz421@users.noreply.github.com>
2024-09-20 08:06:04 -0500

    feat :sparkles: comentarios

--- Dia3/Tarea/RestaurantManager/src/main/java/com/JPA/RestaurantManager/controller/RestaurantController.java
+++ Dia3/Tarea/RestaurantManager/src/main/java/com/JPA/RestaurantManager/controller/RestaurantController.java
@@ -16,49 +16,82 @@ public class RestaurantController {
     @Autowired
     private RestaurantManager restaurantManager;
 
+    
+    //Crear una plato
+    // localhost:8080/restaurant/plato con verbo POST
+    //{
+    // "nombre":"Andres",
+    // "precio":8.99,
+    // "disponibilidad":true
+    //}
     @PostMapping("/plato")
     public ResponseEntity<Void> addPlato(@RequestBody Plato plato) {
         restaurantManager.addPlato(plato);
         return ResponseEntity.ok().build();
     }
-
+    
+    //Obtener todos los platos
+    // localhost:8080/restaurant/platos con verbo GET
     @GetMapping("/platos")
     public List<Plato> getPlatos() {
-        return restaurantManager.listPlatosDisponibles(); // Obtener lista de platos
+        return restaurantManager.listPlatosDisponibles(); 
     }
 
+    //actualizar a un plato por ID
+    // localhost:8080/restaurant/plato/1 con verbo PUT
+    //{
+    // "name":"Andres",
+    // "precio":8.99,
+    // "disponibilidad":true
+    //}
     @PutMapping("/plato/{id}")
     public ResponseEntity<Void> updatePlato(@PathVariable int id, @RequestBody Plato plato) {
         restaurantManager.updatePlato(id, plato);
-        return ResponseEntity.ok().build(); // Respuesta 200 OK
+        return ResponseEntity.ok().build(); 
     }
 
+    //Borra por ID
     @DeleteMapping("/plato/{id}")
     public ResponseEntity<Void> deletePlato(@PathVariable int id) {
         restaurantManager.deletePlato(id);
-        return ResponseEntity.ok().build(); // Respuesta 200 OK
+        return ResponseEntity.ok().build(); 
     }
 
+    //Crear una mesa
+    // localhost:8080/restaurant/mesa con verbo POST
+    //{
+    // "numeroMesa":1,
+    // "ocupada":true
+    //}
     @PostMapping("/mesa")
     public ResponseEntity<Void> addMesa(@RequestBody Mesa mesa) {
         restaurantManager.addMesa(mesa);
-        return ResponseEntity.ok().build(); // Respuesta 200 OK
+        return ResponseEntity.ok().build(); 
     }
 
+    //Obtener todos los mesas
+    // localhost:8080/restaurant/mesas con verbo GET
     @GetMapping("/mesas")
     public List<Mesa> getMesas() {
-        return restaurantManager.listEstadoMesas(); // Obtener lista de mesas
+        return restaurantManager.listEstadoMesas(); 
     }
 
+    //actualizar una mesas por ID
+    // localhost:8080/restaurant/mesa/1 con verbo PUT
+    //{
+    // "numeroMesa":1,
+    // "ocupada":true
+    //}
     @PutMapping("/mesa/{id}")
     public ResponseEntity<Void> updateMesa(@PathVariable int id, @RequestBody Mesa mesa) {
         restaurantManager.updateMesaEstado(id, mesa.isOcupada());
-        return ResponseEntity.ok().build(); // Respuesta 200 OK
+        return ResponseEntity.ok().build(); 
     }
 
+    //Borra por ID
     @DeleteMapping("/mesa/{id}")
     public ResponseEntity<Void> deleteMesa(@PathVariable int id) {
         restaurantManager.deleteMesa(id);
-        return ResponseEntity.ok().build(); // Respuesta 200 OK
+        return ResponseEntity.ok().build();
     }
 }
